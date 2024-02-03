22b368ee8c7de84607ed802e13084c2bcf639a7a
Aleikson <aleiksonsilva@hotmail.com>
2024-02-02 23:08:03 -0300

    Added load introduction

---
+++ src/components/load/Load.jsx
@@ -0,0 +1,11 @@
+import './load.scss';
+
+export const Load = () => {
+  return (
+    <div className='pre-page'>
+      <div className='logo-circle'>
+        <h1>Welcome</h1>
+      </div>
+    </div>
+  );
+};
